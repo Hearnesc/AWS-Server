d5850360b9eb399e0caa39ddd946103a25683c64
Ubuntu <ubuntu@ip-172-31-14-139.us-east-2.compute.internal>
2020-09-26 03:11:08 +0000

    Added some different pages and basic login

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from collections import Counter
 
 app = Flask(__name__)
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return 'Hello from Flask!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid Credntials.'
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html') 
 
 @app.route('/countme/<input_str>')
 def count_me(input_str):
